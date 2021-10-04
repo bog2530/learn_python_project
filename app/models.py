da0da3685440938514d91befefe9041bc0016c54
Bogdan Shumskiy <bog2530@gmail.com>
2021-10-04 08:06:25 +0300

    убрал токен

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    token = db.Column(db.String(128))
     username = db.Column(db.String(60), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
